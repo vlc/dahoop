275baf5f157a84318d065176eee080dbf016c15e
Nick Partridge <nkpart@gmail.com>
2015-07-02 12:05:26 +1000

    .

--- ci_win.ps1
+++ ci_win.ps1
@@ -5,27 +5,22 @@ $msys = $env:MSYS
 if (!$msys) {throw "MSYS environment variable not set"}
 
 $mingw_dir = "$msys\mingw64"
-
 $env:Path += ";$mingw_dir\bin"
-
-echo $env:Path
 
 $packages = @("mingw-w64-x86_64-zeromq")
 
 # ----------------
-
-#####
 echo "Installing Pacman Package Deps"
-#####
 
 $packages |`
   ForEach-Object {
     & $msys\usr\bin\pacman -S -q $_ --needed --noconfirm
   }
 
-####
+# ----------------
 echo "Building with Stack"
-& stack path
+
+& stack clean
 & stack build --extra-include-dirs=$msys/mingw64/include --extra-lib-dirs=$msys/mingw64/lib
 if (!$?) { throw "Build failed" }
 & stack test --extra-include-dirs=$msys/mingw64/include --extra-lib-dirs=$msys/mingw64/lib
